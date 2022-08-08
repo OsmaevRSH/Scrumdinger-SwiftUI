175d56f95eb96666a32259564c753bbf2f7d1797
Osmaev Ruslan <osmaevrsh@gmail.com>
2022-08-08 16:35:52 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Осмаев Руслан on 08.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
